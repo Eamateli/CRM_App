40ada962f5271bcceaca86845a7fd7ba901704f2
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-01 19:16:36 +0200

    Register user: Adding the register_userfunction

--- website/views.py
+++ website/views.py
@@ -24,3 +24,7 @@ def logout_user(request):
     logout(request)
     messages.success(request,"You are Logged Out. See you soon...")
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
+    
