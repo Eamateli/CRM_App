e5045ce330ea4a06726de714d513c80c4ec9fab9
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-02 19:42:22 +0200

    Database model: Updating the models.py and making migrations

--- website/models.py
+++ website/models.py
@@ -11,5 +11,9 @@ class Record(models.Model):
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=20)
+    
+    
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
 
     
